77942715c557598e6fceef84bf0247a5f687cb1a
professorisidro <isidro@professorisidro.com.br>
2024-06-20 21:36:01 -0300

    several content types

--- Chapter03/simpleWebServer/src/br/com/isiflix/simplewebserver/WebServer.java
+++ Chapter03/simpleWebServer/src/br/com/isiflix/simplewebserver/WebServer.java
@@ -1,13 +1,16 @@
 package br.com.isiflix.simplewebserver;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -54,8 +57,6 @@ public class WebServer {
 		String requestMethod = null;
 		Map<String, String> params = new HashMap<String, String>();
 
-		
-
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			mainRequest = br.readLine();
@@ -69,6 +70,7 @@ public class WebServer {
 			// tratar os possíveis cabeçalhos
 			do {
 				tmpLine = br.readLine();
+				WebLogger.log(tmpLine);
 
 			} while (!tmpLine.isBlank());
 
@@ -79,6 +81,7 @@ public class WebServer {
 
 		catch (Exception ex) {
 			WebLogger.error("Problems handling request from " + socket.getInetAddress().getHostAddress());
+			ex.printStackTrace();
 		}
 	}
 
@@ -88,25 +91,30 @@ public class WebServer {
 
 			try {
 
-				PrintWriter pr = new PrintWriter(socket.getOutputStream());
-				FileReader f = new FileReader(completePath);
-				if (f != null) {
+				OutputStream out = socket.getOutputStream();
+				String extension = completePath.substring(completePath.lastIndexOf(".")+1);
+				WebLogger.log("File Name = "+completePath+ " - Extension:"+extension);
+				if (Files.exists(getFilePath(completePath))) {
 					byte[] content = Files.readAllBytes(Paths.get(completePath));
-					String fullContent = new String(content);
+					//String fullContent = new String(content);
 
-					pr.println("HTTP/1.1 200 OK");
-					pr.println("Date: " + LocalDate.now().toString());
-					pr.println("Content-Type: text/html");
-					pr.println("Content-Length: " + fullContent.length());
-					pr.println("");
-					pr.println(fullContent);
-					f.close();
-					pr.close();
+					out.write("HTTP/1.1 200 OK\r\n".getBytes());
+					out.write(("Date: " + LocalDate.now().toString()+"\r\n").getBytes());
+					out.write(("Content-Type:"+WebConfig.contents.get(extension)+"\r\n").getBytes());
+					out.write(("Content-Length: " + content.length+"\r\n").getBytes());
+					out.write("\r\n".getBytes());
+					out.write(content);
+					out.write("\r\n\r\n".getBytes());
+					out.flush();
+					out.close();
 
-				} 					
+				}
+				else {
+					generate404(socket);
+				}
 
 			} catch (Exception ex) {
-				generate404(socket);
+				
 				WebLogger.error(ex.getMessage());
 			}
 		}
@@ -114,27 +122,33 @@ public class WebServer {
 	}
 
 	private String extractPath(String request) {
-		return request.split(" ")[1];
+		String path = request.split(" ")[1];
+		if (path.contains("?")) {
+			return path.substring(0, path.indexOf("?"));
+		}
+		return path;
 	}
 
+	private Path getFilePath(String fullPath) {
+		return Paths.get(fullPath);
+	}
+	
 	private String extractHttpMethod(String request) {
 		return request.split(" ")[0];
 	}
 
-	
 	private void generate404(Socket socket) {
 		try {
 			PrintWriter pr = new PrintWriter(socket.getOutputStream());
 			String message = "<html><body><h1>Not Found!</h1></body></html>";
 			pr.println("HTTP/1.1 404 Not Found");
-			pr.println("Date: "+ LocalDate.now().toString());
+			pr.println("Date: " + LocalDate.now().toString());
 			pr.println("Content-Type: text/html");
-			pr.println("Content-Length: "+message.length());
+			pr.println("Content-Length: " + message.length());
 			pr.println("");
 			pr.println(message);
 			pr.close();
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			WebLogger.error(ex.getMessage());
 		}
 	}
--- Chapter03/simpleWebServer/src/br/com/isiflix/simplewebserver/util/WebConfig.java
+++ Chapter03/simpleWebServer/src/br/com/isiflix/simplewebserver/util/WebConfig.java
@@ -1,6 +1,21 @@
 package br.com.isiflix.simplewebserver.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WebConfig {
-//	public static final String DOCUMENT_ROOT="/Users/isidro/Documents/mywebserver";
-	public static final String DOCUMENT_ROOT="C:\\Users\\isidro\\Documents\\mywebserver";
+	public static final String DOCUMENT_ROOT="/Users/isidro/Documents/mywebserver";
+	//public static final String DOCUMENT_ROOT="C:\\Users\\isidro\\Documents\\mywebserver";
+	
+	
+	public static Map<String, String> contents;
+	
+	public static void setup() {
+		contents = new HashMap<String, String>();
+		contents.put("html","text/html");
+		contents.put("htm","text/html");
+		contents.put("jpg","image/jpg");
+		contents.put("png","image/png");
+		contents.put("jpeg","image/jpeg");
+	}
 }
